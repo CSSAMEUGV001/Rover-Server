c23d22a943162de607140d405cb05910ca050b72
Farid Chahla <Farid.Chahla@Gmail.com>
2016-07-06 12:00:44 -0700

    Switched to i2c from serial

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
-from serial_backend import PyCar
+from i2c_backend import PyCar
 
 car = PyCar()
 
@@ -22,11 +22,7 @@ def value_changed(message):
     steering = int(message.get('steering', 90))
     throttle = int(message.get('throttle', 90))
     car.control(steering, throttle)
-    # print(steering + " " + throttle)
-
-# @socketio.on('connected')
-# def client_connected(message):
-# 	print("Connection from: " + message.get(''))
+    # print(str(steering) + " " + str(throttle))
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0')
