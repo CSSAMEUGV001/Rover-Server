6196551b8d6b64df3cc6b906136501f043b1137a
Farid Chahla <Farid.Chahla@Gmail.com>
2016-07-22 14:39:14 -0700

    Changed to python3 and fixed whitespace errors

--- gps.py
+++ gps.py
@@ -2,15 +2,15 @@ import serial
 import time
 
 class gps:
-    DEFAULT_SERIAL_PATH = '/dev/ttyUSB0'
+    DEFAULT_SERIAL_PATH = '/dev/ttyUSB2'
 
-    def __init__(self, serial_port = DEFAULT_SERIAL_PATH, baud = 9600):
+    def __init__(self, serial_port = DEFAULT_SERIAL_PATH, baud = 115200):
         self.port = serial_port
         self.baud = baud
 
         self.gpsOn = False
 		
-		self.connect()
+        self.connect()
 
 #        if msg == "OK":
 #            print "Connected"
@@ -20,22 +20,22 @@ class gps:
 #            self.connected = False
 
     def connect(self):
-        self.ser = serial.Serial(serial_path, baud)
+        self.ser = serial.Serial(self.port, self.baud)
         while msg != "OK":
             self.ser.write("at\n")
             msg = self.ser.readline()
-        print "Connected to module, OK received"
+        print("Connected to module, OK received")
 
 	#Gets raw gps data returned from gps module
     def getGPS(self):
-		msg = "ERROR"
+        msg = "ERROR"
         if self.connected and self.gpsOn:
-			while True:
-				self.ser.write("At+CGPSINFO")
-				time.sleep(1)
-				msg = self.ser.readline()
-				if msg.find("+CGPSINFO:") != -1:
-					break
+            while True:
+                self.ser.write("At+CGPSINFO")
+                time.sleep(1)
+                msg = self.ser.readline()
+                if msg.find("+CGPSINFO:") != -1:
+                    break
         return msg	
 			
 	#Turns the gps module on
@@ -44,7 +44,7 @@ class gps:
             while self.gpsOn == False:
                 self.ser.write("AT+CGPS=1")
                 msg = self.ser.readline()
-                if msg == "OK"
+                if msg == "OK":
                     self.gpsOn = True
 
 	#Turns the gps module off
@@ -56,22 +56,22 @@ class gps:
     # TODO, add parsing to just get lat and long
     def getLongLat(self):
 		#Makes sure there is a serial connection, if not will connect
-		while !self.connected:
-			self.connect()
-			time.sleep(2)
+        while not self.connected:
+            self.connect()
+            time.sleep(2)
 			
 		#Makes sure gps is on, if not turns on
-		while !self.gpsOn:
-			self.startGPS()
-			time.sleep(2)
-		
+        while not self.gpsOn:
+            self.startGPS()
+            time.sleep(2)
+
         return getGPS()
 	
 	# Universal parser to parse AT command responses
-	def parse_reply(self, message, seperator):
-		msgList = message.split(seperator)
-		
-		return msgList
+    def parse_reply(self, message, seperator):
+        msgList = message.split(seperator)
+
+        return msgList
 		
 	
 	
